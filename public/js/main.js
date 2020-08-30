3969d0268964f30c42933f1c256da8df6d201de2
Patrick Stankard <me@patrickstankard.com>
2020-08-30 16:54:11 -0400

    Update JS for photo navigation

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,90 @@ function setContentWrapperMinHeight() {
     $('#content-wrapper').css('min-height', contentMinHeight);
 }
 
+function onPhotoModalShow(event) {
+    const anchor = $(event.relatedTarget);
+
+    setPhotoModalContent(anchor);
+}
+
+function onPhotoModalKeyDown(event) {
+    const modal = $('#photo-modal');
+    const previousPhotoButton = modal.find('button.btn.photo-previous');
+    const nextPhotoButton = modal.find('button.btn.photo-next');
+
+    const keyCode = (event.keyCode ?? event.which);
+
+    if (keyCode === 37) {
+        previousPhotoButton.click();
+    } else if (keyCode === 39) {
+        nextPhotoButton.click();
+    }
+}
+
+function onPhotoNavigationClick(event) {
+    const button = $(event.target);
+    const anchor = $('a.photo-thumbnail[href="' + button.data('url') + '"]');
+
+    setPhotoModalContent(anchor);
+}
+
+function setPhotoModalContent(anchor) {
+    const modal = $('#photo-modal');
+    const previousPhotoButton = modal.find('button.btn.photo-previous');
+    const nextPhotoButton = modal.find('button.btn.photo-next');
+
+    const href = anchor.attr('href');
+    const previousPhotoUrl = anchor.data('previousPhoto');
+    const nextPhotoUrl = anchor.data('nextPhoto');
+
+    modal.find('a.photo-download').attr('href', href);
+    modal.find('img.photo-preview').attr('src', href);
+
+    if (previousPhotoUrl) {
+        previousPhotoButton.data('url', previousPhotoUrl);
+        previousPhotoButton.attr('disabled', false);
+    } else {
+        previousPhotoButton.attr('disabled', true);
+    }
+
+    if (nextPhotoUrl) {
+        nextPhotoButton.data('url', nextPhotoUrl);
+        nextPhotoButton.attr('disabled', false);
+    } else {
+        nextPhotoButton.attr('disabled', true);
+    }
+
+    modal.modal('handleUpdate');
+}
+
+function setPreviousAndNextPhotos() {
+    let index, currentPhoto, previousPhoto, nextPhoto;
+
+    const photos = $('a.photo-thumbnail');
+    const numPhotos = photos.length;
+
+    for (index = 0; index < numPhotos; index++) {
+        currentPhoto = photos.eq(index);
+
+        if (index > 0) {
+            previousPhoto = photos.eq(index - 1);
+            currentPhoto.data('previous-photo', previousPhoto.attr('href'));
+        }
+
+        if (index < numPhotos - 1) {
+            nextPhoto = photos.eq(index + 1);
+            currentPhoto.data('next-photo', nextPhoto.attr('href'));
+        }
+    }
+}
+
 $(() => {
     setContentWrapperMinHeight();
     $('#footer-wrapper').removeClass('invisible');
-
     $(window).resize(setContentWrapperMinHeight);
+
+    setPreviousAndNextPhotos();
+    $('#photo-modal').on('show.bs.modal', onPhotoModalShow);
+    $('button.btn.photo-previous, button.btn.photo-next').on('click', onPhotoNavigationClick);
+    $('body').on('keydown', '#photo-modal, button.btn.photo-previous, button.btn.photo-next', onPhotoModalKeyDown);
 });
